f1ec455cccee0512f776ae839f78c32d2178bd40
Will Meierjurgen Farr <wmfarr@gmail.com>
2016-10-15 00:26:33 +0100

    Started accumulating ideas about tests to write.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,9 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+# Ideas for tests to implement Any Day Now (TM)
+#
+# 1. Check against full-matrix likelihood.
+# 2. Check that init returns always within prior.
+# 3. Check to_params/to_array are inverses.
